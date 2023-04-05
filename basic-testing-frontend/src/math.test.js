26a9ac74ad53e459a4c173b0180fe95502538c85
Murhussain <husseinmurashi@yahoo.com>
2023-04-05 16:49:51 +0200

    Following AAA pattern to existing test function

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,7 +2,15 @@ import { it, expect } from 'vitest';
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-  const result = add([1, 2, 3]);
+  // Arrange
+  const numbers =  [1,2,3]
+  // Act
+  const result = add(numbers);
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
 
-  expect(result).toBe(6);
+  expect(result).toBe(expectedResult);
 });
